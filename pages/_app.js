4c56c61f85a2b176d5f59fd27c550fd80355015e
siwen.ren <siwen.ren@gaiaworks.cn>
2019-05-09 17:08:58 +0800

    setup basic config

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import App from "next/app";
+import Router from "next/router";
+import * as gtag from "../lib/gtag";
+
+Router.events.on("routeChangeComplete", url => gtag.pageview(url));
+
+export default App;
